e17fba165f94015ba1f5a4811e7d5950ead48537
Ibrahim Mohamed <ibrahimmohamedprof@gmail.com>
2024-10-13 13:05:14 +0000

    update projet flask api platefom

--- app/utils.py
+++ app/utils.py
@@ -1,14 +1,14 @@
 from flask_swagger_ui import get_swaggerui_blueprint
 
 
-SWAGGER_URL = '/swagger'  
+SWAGGER_URL = ''  
 API_URL = '/static/swagger.json'  
 
 swaggerui_blueprint = get_swaggerui_blueprint(
     SWAGGER_URL,  
     API_URL,
     config={  
-        'app_name': "Test application"
+        'app_name': "Api plateforme"
     },
 )
 
